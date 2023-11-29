b4c4b267406451300db858660ae31426a416de45
dMedice <133895157+dMedice@users.noreply.github.com>
2023-11-29 07:19:31 -0300

    Update Departamento.java

--- prjEmpresa/src/main/java/com/senai/prjEmpresa/Entities/Departamento.java
+++ prjEmpresa/src/main/java/com/senai/prjEmpresa/Entities/Departamento.java
@@ -7,15 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "Departamento")
 public class Departamento {
