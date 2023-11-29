2f4096aaa10e179a4a682d9621ea33940a93cfeb
dMedice <133895157+dMedice@users.noreply.github.com>
2023-11-29 07:19:59 -0300

    Update Funcionario.java

--- prjEmpresa/src/main/java/com/senai/prjEmpresa/Entities/Funcionario.java
+++ prjEmpresa/src/main/java/com/senai/prjEmpresa/Entities/Funcionario.java
@@ -9,15 +9,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 
 @Entity
 @Table(name = "Funcionário")
